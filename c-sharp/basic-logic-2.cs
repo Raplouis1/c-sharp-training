09479c90407a575a925ce21bd7d44dbddf62b70d
Raphael Torres <contato.raphaeltorres@gmail.com>
2016-12-05 00:42:52 -0200

    Update basic-logic-2.cs

--- c-sharp/basic-logic-2.cs
+++ c-sharp/basic-logic-2.cs
@@ -1,36 +1,105 @@
-//More basic programming logic training with C#.
+//Programming logic training with C#.Control flow - if/else
 
 //-----------------------------------------------------------
 
-/* 1-Make a program that calculates how much money a person spent in cigarettes.Use the number of years that person smoke, 
-number of cigarettes smoke by day and the price of a single pack(with 20 cigarettes). */
+/* 1-Make a program to solve a quadratic equation(x² + bx + c = 0).The program asks for the user to enter the value
+ of a, b and c. Note: Delta(discriminant) = b² - 4ac and Bhaskara = (-b ±√Delta) / 2a  */
 
-int yearsSmoking; //Example: 10 years
-int numberOfCigarettesSmokePerDay; //Example: 15 cigarettes a day
-double priceOfAPackOfCigarettes; //Example: 5.25
-double numberOfCigarettesSmokeInAYear; 
-double packsSmokedInAYear;
-double moneySpentInCigarettesInAYear;
-double moneySpentInCigarettesEntireLife;
+ double a, b, c, x1, x2;
+ double delta;
+ 
+ Console.WriteLine("Write the 'a' value:");
+ a = Convert.ToDouble(Console.ReadLine()); //Example: a = 1
+ Console.WriteLine("Write the 'b' value:"); 
+ b = Convert.ToDouble(Console.ReadLine()); //Example: b = 6
+ Console.WriteLine("Write the 'c' value:");
+ c = Convert.ToDouble(Console.ReadLine()); //Example: c = 2
 
-Console.WriteLine("How many years have you been smoking?");
-yearsSmoking = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("How many cigarettes do you smoke per day?");
-numberOfCigarettesSmokePerDay = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("How much costs a pack of cigarette?");
-priceOfAPackOfCigarettes = Convert.ToDouble(Console.ReadLine());
+ delta = Math.Pow(b,2) - ( 4 * a * c ); // delta = 6² - (4 * 1 * 2) = 36 - 8 = 28
+ x1 = ((-1 * b) + Math.Sqrt(delta)) / (2*a); // x1 = -0.4
+ x2 = ((-1 * b) - Math.Sqrt(delta)) / (2*a); // x2 = -5.6
+   
+ if (delta  > 0) 
+ {
+  Console.WriteLine("This equation has two solutions.");
+  Console.WriteLine("Delta is equal to " + delta);
+  Console.WriteLine("X1 is " + x1 + " and " + "X2 is " + x2);
+ }
+ else if(delta == 0)
+ {
+   Console.WriteLine("This equation has only one solution.");
+   Console.WriteLine("Delta is equal to " + delta);
+   Console.WriteLine("X is " + x1);
+ }
+ else
+ {
+    Console.WriteLine("Delta is negative.This equation has no solution.");
+ }
 
-numberOfCigarettesSmokeInAYear = numberOfCigarettesSmokePerDay * 365; // 15 * 365 = 5475 cigarettes per year
-packsSmokedInAYear = numberOfCigarettesSmokeInAYear / 20; // 5475 / 20 = 273.75 packs
-moneySpentInCigarettesInAYear = packsSmokedInAYear * priceOfAPackOfCigarettes; //273.75 * 5.25 = 1437.1875
-moneySpentInCigarettesEntireLife = moneySpentInCigarettesInAYear * yearsSmoking; //1437.1875 * 10 = 14371.875
+//--------------------------------------------------------
 
-Console.WriteLine("You spend, in a year: " + moneySpentInCigarettesInAYear); //You spend, in a year: 1437.1875
-Console.WriteLine("You have spent, in " + yearsSmoking + " years: " + moneySpentInCigarettesEntireLife); /*You have spent, in 10
-years: 14371.875*/
-Console.WriteLine("Be smart.Stop smoking!!!");
+// 2-Make a program that reads a whole number and tells if it is multiple of 3 and 4 at the same time.
 
-//-------------------------------------------------------
+ int number;
+ int multipleVerification3;
+ int multipleVerification4;
+
+ Console.WriteLine("Write a whole number:");
+ number = Convert.ToInt32(Console.ReadLine()); //Example: 12
+ multipleVerification3 = number % 3; //12 % 3 = 0
+ multipleVerification4 = number % 4; //12 % 4 = 0
+
+ if(multipleVerification3 == 0 && multipleVerification4 == 0) //Both conditions are true
+ {
+   Console.WriteLine(number + " is multiple of both 3 and 4."); //Shows this message
+ }
+ else
+ {
+   Console.WriteLine(number + " is not multiple of both 3 and 4");
+ }
+
+//--------------------------------------------------------
+
+/* 3-A hotel charges $60 per day plus service tax.The service tax is:
+   $55 per day, if the customer stays for more than 15 days;
+   $60 per day, if the customer stays exactly 15 days; 
+   $80 per day, if the customer stays less than 15 days;
+   Write a program that shows the costumer name and bill.
+*/
+
+ string name, surname;
+ int numberOfDaysStayed;
+ double serviceTax, bill;
+ double chargePerDay = 60;
+ 
+ Console.WriteLine("Write the costumer's name and surname:");
+ name = Console.ReadLine();
+ surname = Console.ReadLine();
+ Console.WriteLine("Write how long the costumer stayed:");
+ numberOfDaysStayed = Convert.ToInt32(Console.ReadLine());
+ 
+ 
+ if(numberOfDaysStayed > 15)
+ {
+  serviceTax = 55;  
+ }
+ else if(numberOfDaysStayed == 15)
+ {
+  serviceTax = 60;  
+ }
+ else
+ {
+   serviceTax = 80;   
+ } 
+ 
+ bill = chargePerDay + serviceTax * numberOfDaysStayed;
+ Console.WriteLine("Mr's/Ms'"+ surname + " bill is: $" + bill);
+ 
+  
+   
+ 
+
+
 
 
 
