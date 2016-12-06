5a2802d7469494141de8e720a6bc5002978f6375
Raphael Torres <contato.raphaeltorres@gmail.com>
2016-12-05 23:57:18 -0200

    Update basic-logic-3.cs

--- c-sharp/basic-logic-3.cs
+++ c-sharp/basic-logic-3.cs
@@ -1,1 +1,20 @@
 // Programming logic training with C# - Control flow - while / do-while
+
+//-----------------------------------------------------------
+
+// 1-Write a program that shows odd numbers between 1 and 100
+ int i = 0;
+ int control;
+
+ while( i >= 1 && i < 100)
+ {
+   i++;
+   if(control != 0)
+   {
+     Console.WriteLine(i);
+   }
+ }
+
+//-----------------------------------------------------------
+
+ 
