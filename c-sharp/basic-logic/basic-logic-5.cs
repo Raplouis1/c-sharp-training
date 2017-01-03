ff9ab97b2a3a16d8cd01853609dd72868c2ca4b1
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-01-03 15:44:57 -0200

    Update basic-logic-5.cs

--- c-sharp/basic-logic/basic-logic-5.cs
+++ c-sharp/basic-logic/basic-logic-5.cs
@@ -2,3 +2,40 @@
 
 //-----------------------------------------------------------------------
 
+            char operation;
+            float a;
+            float b;
+            float result;            
+
+            Console.WriteLine("Write 2 numbers:");
+            Console.WriteLine("Write the first number:");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Write the second number:");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Now, choose one of the four basic math operations(addition (+), subtraction (-), multiplication (*) or division (/) ).Type ");
+            Console.WriteLine("Use one of the above symbols(+ , - , * or /)");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    result = a + b;
+                    Console.WriteLine(result);
+                    break;
+                case '-':
+                    result = a - b;
+                    Console.WriteLine(result);
+                    break;
+                case '*':
+                   result = a * b;
+                    Console.WriteLine(result);
+                    break;
+                case '/':
+                    result = a / b;
+                    Console.WriteLine(result);
+                    break;
+                default :
+                    Console.WriteLine("Wrong key.Type it again.");
+                    break;
+            }
+
