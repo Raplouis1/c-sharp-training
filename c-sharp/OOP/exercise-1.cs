945b6eb40a18368f15403c215abafe4cdc45d3f9
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-01-04 20:13:19 -0200

    Create exercise-1.cs

---
+++ c-sharp/OOP/exercise-1.cs
@@ -0,0 +1,18 @@
+/*Here, I train the basic OOP concepts:
+ What is:
+ -an object;
+ -a class;
+ -a method.
+ 
+*/
+
+//------------------------------------------------------------
+
+/* 1-Use the object PEN as an example to this exercise.Classify the pen (show it's attributes and methods ), then create, in C#, 
+ a basic program to shows it's classifications(don't forget to create it's class and the object). */
+ 
+class Pen
+{
+
+
+}
