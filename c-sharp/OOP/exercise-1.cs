06b5b26f8eafc8d508d23ed88673a8b160a27648
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-01-04 22:08:32 -0200

    Update exercise-1.cs

--- c-sharp/OOP/exercise-1.cs
+++ c-sharp/OOP/exercise-1.cs
@@ -9,7 +9,7 @@
 //=======================================================================
 //=======================================================================
 
-/* 1-Use the object PEN as an example to this exercise.Classify the pen (show it's attributes and methods ), then create, in C#, 
+/*Use the object PEN as an example to this exercise.Classify the pen (show it's attributes and methods ), then create, in C#, 
  a basic program to shows it's classifications(don't forget to create it's class and the object). */
 
 /*
