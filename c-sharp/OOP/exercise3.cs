4dfbebd58cefedba3508a604391ada02004e2830
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-01-18 18:56:43 -0200

    Update exercise3.cs

--- c-sharp/OOP/exercise3.cs
+++ c-sharp/OOP/exercise3.cs
@@ -1,4 +1,4 @@
-/*Visibility: indicates the level of access of the class' internal components (methods and attributes), i.e., how this items
+/*Visibility (or access): indicates the level of access of the class' internal components (methods and attributes), i.e., how this items
  can be accessed.
  
  Using UML, we can set a visual class diagram to be easy to represent the class and it's visibility:
@@ -19,5 +19,30 @@
   |lidOff()   |
   -------------
   
-  Visibility modifiers:
+  UML Visibility modifiers:
+  In the UML, there are 3 symbols that indicates the visibility:
+  + public - the component can be accessed by any other components.
+  - private - the component can only be accessed by the other components in the same class.
+  # protected - the component can only be accessed by the other components in the same class (and this class' subclasses).  
+  
+  Example:
+  
+  ------------
+  |Pen        | Class name
+  ------------  
+  |+model      | Attributes
+  |+color      |
+  |-tipSize    |
+  |#penLoad    |
+  |#lid        |
+  -------------
+  |+write()    | Methods
+  |+scribble() |
+  |+paint()    |
+  |-lidOn()    |
+  |-lidOff()   |
+  -------------
+
 */
+
+
