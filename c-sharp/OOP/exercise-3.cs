19a50b5a54e50a48f54108a6be6b20d603a0ba1a
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-01-18 18:45:30 -0200

    Create exercise-3.cs

---
+++ c-sharp/OOP/exercise-3.cs
@@ -0,0 +1,23 @@
+/* Visibility: indicates the level of access of the class' internal components (methods and attributes), i.e., how this items
+ can be accessed.
+ 
+ Using UML, we can set a visual class diagram to be easy to represent the class and it's visibility:
+ Example:
+  ------------
+  |Pen        | Class name
+  ------------  
+  |model      | Attributes
+  |color      |
+  |tipSize    |
+  |penLoad    |
+  |lid        |
+  -------------
+  |write()    | Methods
+  |scribble() |
+  |paint()    |
+  |lidOn()    |
+  |lidOff()   |
+  -------------
+  
+  Visibility modifiers:
+*/
