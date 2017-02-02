a96729a1928a43a087e6298a781c1464caa5168f
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-02-02 14:06:29 -0200

    Update mini-project-1.cs

--- c-sharp/OOP/mini-project-1.cs
+++ c-sharp/OOP/mini-project-1.cs
@@ -2,3 +2,11 @@
 
 //======================================================
 
+/*
+Two people (Mary and John) want to put money in a bank account.John opened a savings account to deposit his 300U$ and Mary opened a 
+checking account to deposit 500U$ and then, withdrew 100$ to buy a pair of shoes.
+A bank account has the following attributes: account number, account type(savings or checking), client name, balance, status(opened/closed).
+And it has the following methods: open account, close account, deposit, withdraw, pay monthly fee, 
+constructor new account(status=false, balance=0) and the getters/setters.
+
+
