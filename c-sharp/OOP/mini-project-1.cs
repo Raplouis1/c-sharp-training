09c6d653a29d42849556837ea0cf785da098c74d
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-02-06 10:01:43 -0200

    Update mini-project-1.cs

--- c-sharp/OOP/mini-project-1.cs
+++ c-sharp/OOP/mini-project-1.cs
@@ -3,28 +3,33 @@
 //======================================================
 
 /*
-Two people (Mary and John) want to put money in a bank account.John opened a savings account to deposit his 300U$ and Mary opened a 
-checking account to deposit 500U$ and then, withdrew 100$ to buy a pair of shoes.
+Two people (Mary and John) want to put some money in their bank account.John opened a savings account to deposit his 300U$ and Mary opened 
+a checking account to deposit 500U$ and then, withdrew 100$ to buy a pair of shoes.
 Each account have the following attributes and methods:
+
 BankAccount //Class name
-//Attributes
+
+//Attributes (+public / # protected / - private)
 + account number
 # type(checkings/savings)
 - client name
 - balance
 - status(true->account is open / false->account is closed)
-//Methods
-open account(turns 'status' into true / if chose checkings += 50 / if chose savings +=150)
-close account(turns 'status' into false / can't have money in the account or have debits)
-deposit(status must be true)
-withdraw(status must be true / balance must be >= value )
-pay monthly fee(checkings=12 / savings=20)
-constructor new account(status = false, balance = 0)
-getters/setters
 
-So, write a program to create and manage the clients account.
+//Methods (in this example, all methods are public)
+• open account (turns 'status' into true / if chose checkings += 50 / if chose savings +=150)
+• close account (turns 'status' into false / can't have money in the account or have debits)
+• deposit (status must be true)
+• withdraw (status must be true / balance must be >= value )
+• pay monthly fee (checkings=12 / savings=20)
+• constructor new account (status = false, balance = 0)
+• getters/setters
+
+So, write a program to create and manage the clients' accounts.
 
 //======================================================
+
+
 
 
 
