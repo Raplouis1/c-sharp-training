7a7c5b4bbb4425663953a55d7c2b9d222af99701
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-02-06 10:11:38 -0200

    Update mini-project-1.cs

--- c-sharp/OOP/mini-project-1.cs
+++ c-sharp/OOP/mini-project-1.cs
@@ -3,8 +3,8 @@
 //======================================================
 
 /*
-Two people (Mary and John) want to put some money in their bank account.John opened a savings account to deposit his 300U$ and Mary opened 
-a checking account to deposit 500U$ and then, withdrew 100$ to buy a pair of shoes.
+Two people (Mary and John) want to put some money in their bank account.John opened a savings account to deposit his 300U$ and
+Mary opened a checking account to deposit 500U$ and then, withdrew 100$ to buy a pair of shoes.
 Each account have the following attributes and methods:
 
 BankAccount //Class name
@@ -20,7 +20,7 @@ BankAccount //Class name
 • open account (turns 'status' into true / if chose checkings += 50 / if chose savings +=150)
 • close account (turns 'status' into false / can't have money in the account or have debits)
 • deposit (status must be true)
-• withdraw (status must be true / balance must be >= value )
+• withdraw (status must be true / balance must be >= value)
 • pay monthly fee (checkings=12 / savings=20)
 • constructor new account (status = false, balance = 0)
 • getters/setters
@@ -28,11 +28,22 @@ BankAccount //Class name
 So, write a program to create and manage the clients' accounts.
 
 //======================================================
+*/
+public class BankAccount
+{
+  public int accountNumber;
+  protected string accountType;
+  private string clientName;
+  private int balance;
+  private bool status;
+  
+  
+}
 
+//Main class
 
-
-
-
-
-
-
+class Program
+{
+  
+  
+}
