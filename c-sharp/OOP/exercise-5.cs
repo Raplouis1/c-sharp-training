606956ff0228f679612dbae1f71a9b3e4f9a7394
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-02-10 14:25:16 -0200

    Update exercise-5.cs

--- c-sharp/OOP/exercise-5.cs
+++ c-sharp/OOP/exercise-5.cs
@@ -1,3 +1,11 @@
-//Encapsulation (Interfaces) training
+//Encapsulation (Interfaces) 
+
+/* •Encapsulation: It's important to protect your code from the user (or a programmer) and the user from the code.
+ As a real world example, we can use a AAA battery.The battery has inside it, toxic components(zinc, magnesium oxide, etc) that
+ are dangerous for the human's health. So, the manufacturer uses a protective case to wrap the battery, avoiding the costumers 
+ having direct contact with those chemicals. 
+ In OOP, that case (or capsule) can "talk" to the outer world. INTERFACE. through messages and the    
+ 
+ Below, we have a clear example, a TV remote control:
 
 //==========================================================
