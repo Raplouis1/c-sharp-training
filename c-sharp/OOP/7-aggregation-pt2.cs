4c411cdeb56aaa0ed871cc743ce00f4c5843dd4e
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-02 16:35:00 -0300

    Update 7-aggregation-pt2.cs

--- c-sharp/OOP/7-aggregation-pt2.cs
+++ c-sharp/OOP/7-aggregation-pt2.cs
@@ -3,13 +3,13 @@
 •Aggregation: it's a special kind of classes relationship.Example(based on the previous exercise):
  w[0] = new Warrior("Ninja", "Japan", 31, 1.75f, 68.9f, 11, 2, 1); //Warrior 1
  w[1] = new Warrior("Capoeira", "Brazil", 29, 1.68f, 57.8f, 14, 2, 3); //Warrior 2
- We need to create a class to make them fight.The class is called "Fight":
+-We need to create a class to make them fight.The class is called "Fight":
  -Class: Fight
  -Attributes(- is private ): 
   -adversary(who was challenged): abstract 
   -challenger(who asked for a fight): abstract
   -rounds: int
-  -fightApproved: bool //Show if this fights is according with the tournament rules
+  -fightApproved: bool //Show if this fights is according with the tournament rules(see it below)
  -Methods:
   +ArrangeFight()
   +FightNow()
@@ -20,6 +20,12 @@
    Warrior ---------------------------------------------------<> Fight
                 (<--a Fight is disputed by Warrior(s)<--)
    i.e. class B(Fight) has one (or more) attributes from class A(Warrior).This is aggregation.
+   
+   -Tournament rules:
+    1-Fights can only be arranged by Warrior from the same category
+    2-Challenger and adversary must be different
+    3-Fight should only be started if it's approved
+    4-Fight result can only be: one warrior won or the fight drew
 
 */
 
@@ -36,8 +42,15 @@ class Fight
  private bool fightApproved;
  
  //Methods
- public void ArrangeFight()
+ public void ArrangeFight(w[0], w[1])
  {
+  if(w[0].WarriorClass = w[1].WarriorClass && w[0] != w[1])
+  {
+   fightApproved = true;
+   adversary = w[0];
+   challenger = w[1];
+  }
+  
  }
  public void FightNow()
  {
