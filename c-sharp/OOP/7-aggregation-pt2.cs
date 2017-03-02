637eda8b2a42080cbea10a6b1ebd105bb9c90e0f
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-02 15:04:10 -0300

    Update 7-aggregation-pt2.cs

--- c-sharp/OOP/7-aggregation-pt2.cs
+++ c-sharp/OOP/7-aggregation-pt2.cs
@@ -1,1 +1,10 @@
 //Aggregation part 2
+/*
+•Aggregation: it's a special kind of classes relationship.Example(based on the previous exercise):
+ w[0] = new Warrior("Ninja", "Japan", 31, 1.75f, 68.9f, 11, 2, 1); //Warrior 1
+ w[1] = new Warrior("Capoeira", "Brazil", 29, 1.68f, 57.8f, 14, 2, 3); //Warrior 2
+ We need to create a class to make them fight.The class is called "Fight":
+ 
+ 
+
+*/
