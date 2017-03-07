94b2c0d32cad66d087e272018ae85b53167d024e
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-07 16:31:23 -0300

    Update mini-project-2.cs

--- c-sharp/OOP/mini-project-2.cs
+++ c-sharp/OOP/mini-project-2.cs
@@ -1,1 +1,33 @@
-//
+/*
+ Using all the concepts until the exercise 7 (Part 2) - Aggregation, we are going to create a 
+ program with the following classes and interface:
+ 
+ Class: Person
+ -Attributes:
+  -name
+  -age
+  -sex
+ -Methods:
+  +Birthday
+  getters/setters/constructor
+  
+  Class: Book (aggregated from Person)
+  -Attributes:
+   -title
+   -author
+   -pages
+   -currentPage
+   -open
+   -reader
+  -Methods:
+   +Details
+   getters/setters/constructor
+   
+   Interface: Issue (implemented by Book)
+   -Methods:
+    +Open
+    +Close
+    +Browse
+    +NextPage
+    +ReturnPage
+*/
