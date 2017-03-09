fe2e968ff09be65bb542ab7839ecf9317b946377
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-09 15:39:13 -0300

    Update 8-inheritance-pt1.cs

--- c-sharp/OOP/8-inheritance-pt1.cs
+++ c-sharp/OOP/8-inheritance-pt1.cs
@@ -1,1 +1,8 @@
-//==================================================================
+/*
+•Inheritance: allows a new class to receive the characteristics (inherit) from an class that already exists.
+ -Example: A son that has the eyes of his mother.
+•The characteristics can be the attributes and the methods.
+
+
+*/
+//=================================================================
