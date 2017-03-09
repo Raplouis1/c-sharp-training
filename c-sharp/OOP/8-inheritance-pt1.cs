675fd25e48a6b3a1a84ed6f69f0a499675468a82
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-09 16:14:11 -0300

    Update 8-inheritance-pt1.cs

--- c-sharp/OOP/8-inheritance-pt1.cs
+++ c-sharp/OOP/8-inheritance-pt1.cs
@@ -1,7 +1,47 @@
 /*
-•Inheritance: allows a new class to receive the characteristics (inherit) from an class that already exists.
+•Inheritance: allows a new class to receive the characteristics* (inherit) from an class that already exists.
  -Example: A son that has the eyes of his mother.
-•The characteristics can be the attributes and the methods.
+*The characteristics can be the attributes and the methods.
+
+-Example 2:
+ -People in a school: a student, a professor and a secretary (Classes).
+ 
+  •Student:
+   name
+   age
+   sex 
+   studentNumber
+   course
+   Birthday()
+   Study()
+   
+  •Professor:
+   name
+   age
+   sex
+   speciality
+   salary
+   Birthday()
+   GiveClass()
+   
+  •Secretary:
+   name
+   age
+   sex
+   department
+   working
+   Birthday()
+   StopWorking()   
+   
+  -Note that the three classes has common characteristics (name, age, sex and Birthday() ).So, through that, we can
+   know that they have a "mother" class called Person that owns all that characteristics that they inherit:
+ 
+   •Person:
+    name
+    age
+    sex
+    Birthday()
+   
 
 
 */
