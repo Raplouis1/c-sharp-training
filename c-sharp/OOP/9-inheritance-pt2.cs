61a61a7f15c1126651f071398f0728b983762531
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-10 15:20:05 -0300

    Update 9-inheritance-pt2.cs

--- c-sharp/OOP/9-inheritance-pt2.cs
+++ c-sharp/OOP/9-inheritance-pt2.cs
@@ -13,6 +13,13 @@
 Note: In the example above, the Root of the tree (A) is a class that doesn't have any mother class (or superclass) above it
 and the Leaves(leaf)(ABAA and ABAB) are the classes that dont't have children classes(or sub classes)
 
+Note:
+-More you travel through the tree, more the classes become SPECIALIZED i.e. more detailed the classes become:
+ -Example: Person <----- Student <----- UndergraduateStudent
+  -A Person has: name, age, sex and Birthday();
+  -A Student has all the characteristics from Person plus studentNumber, course and payTuition();
+  -A UndergraduateStudent have all the characteristics from Person + Student plus internship, loan, payLoan(); and gotInternship();
+
 
 */
 //============================================================================
