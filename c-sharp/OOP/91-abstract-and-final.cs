0d58f962ccda0d5e1f3a99d788c06db2885f9aad
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-10 15:40:35 -0300

    Update 91-abstract-and-final.cs

--- c-sharp/OOP/91-abstract-and-final.cs
+++ c-sharp/OOP/91-abstract-and-final.cs
@@ -1,1 +1,12 @@
+/*
+•Abstract and final:
+ -We can have classes and methods abstract or final
+ So:
+ -Abstract class: Can't be instanced i.e. can't make objects.It can only serve as superclass for subclasses. 
+ -Abstract method: Declared but it isn't implemented on the superclass(Example: the interface methods).
+ -Final class: Can't be inherited by other classes.It is a leaf in the uml tree.
+ -Final method: Can't be overrided by subclasses.It can only be inherited.
+ 
+*/
+
 //==================================================================================================
