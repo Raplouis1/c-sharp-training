c10496c9ac4b5b03f156166c609459189221cd3c
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-10 14:54:22 -0300

    Update 9-inheritance-pt2.cs

--- c-sharp/OOP/9-inheritance-pt2.cs
+++ c-sharp/OOP/9-inheritance-pt2.cs
@@ -1,1 +1,14 @@
+/*
+•UML Inheritance Tree:
+
+      |--AC <------ ACA
+ A <-----AA <------ AAA
+(Root)|--AB <------ ABA <----- ABAA 
+                           |-- ABAB
+                               (Leaves(Leaf))
+
+Note: In the example above, the Root of the tree (A) is a class that doesn't have any mother class (or superclass) above it
+and the Leaves(leaf)(ABAA and ABAB) are the classes that dont't have daughter classes(or sub classes)
+
+*/
 //============================================================================
