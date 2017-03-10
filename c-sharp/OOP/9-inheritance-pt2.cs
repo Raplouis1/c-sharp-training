a8f1b9b556013b8d1d00da93b1c7dbcc78e8349f
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-10 15:02:32 -0300

    Update 9-inheritance-pt2.cs

--- c-sharp/OOP/9-inheritance-pt2.cs
+++ c-sharp/OOP/9-inheritance-pt2.cs
@@ -1,14 +1,18 @@
 /*
 •UML Inheritance Tree:
 
+
+-------------------------------------------------------->>> Specialization: "travel" from the top to the bottom  of thre uml tree
       |--AC <------ ACA
  A <-----AA <------ AAA
 (Root)|--AB <------ ABA <----- ABAA 
                            |-- ABAB
                                (Leaves(Leaf))
+<<<-------------------------------------------------------- Generalization: "travel" from the bottom to the top  of thre uml tree                         
 
 Note: In the example above, the Root of the tree (A) is a class that doesn't have any mother class (or superclass) above it
-and the Leaves(leaf)(ABAA and ABAB) are the classes that dont't have daughter classes(or sub classes)
+and the Leaves(leaf)(ABAA and ABAB) are the classes that dont't have children classes(or sub classes)
+
 
 */
 //============================================================================
