5dc7fc324eb1b0fe9901f9111a36abc630e6c023
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-10 16:03:24 -0300

    Update 92-exercise8991.cs

--- c-sharp/OOP/92-exercise8991.cs
+++ c-sharp/OOP/92-exercise8991.cs
@@ -1,1 +1,25 @@
 //==================================================================================
+
+/*
+••Using the concepts learned in the exercises 8, 9 and 91, we're gonna to create a program with the following classes:
+ •Person:
+  -name
+  -age
+  -sex
+  +Birthday()
+ •Visitor:(implements Person)
+  (Only receives the characteristics from Person)
+ •Student:(implements Person)
+  -studentNumber
+  -course
+  +PayTuiton()
+ •StudentWithScholarship: (implements Student and not Person but receives the characteristc of Person through Student)
+  -scholarship
+  +ExtendScholarship()  
+  +PayTuition() @Override(As this student has scholarship, he will pay a different tuition price, so we need to override the previous
+  tuition payment Method).
+  
+  
+ 
+
+*/
