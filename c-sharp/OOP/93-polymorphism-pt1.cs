d6fd9724c8df5fc979f711cac03a68f0532d7f33
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-16 21:03:51 -0300

    Update and rename 93-Polimorfism-pt1.cs to 93-polymorphism-pt1.cs

---
+++ c-sharp/OOP/93-polymorphism-pt1.cs
@@ -0,0 +1,29 @@
+/*
+•Polymorphism: word that comes from Greek language (Poly = "many, much", Morphos = "shape, form") meaning 
+ "something that can have multiple shapes/forms and/or behaviors".
+ 
+ •Method signature: 
+  -Every method can or cannot receive arguments(parameters). The quantity and types of it's arguments will define
+  it's signature.
+  -Every method has it own signature.
+  -Example: 
+   -public void calculateGPA(float n1, float n2)
+    {
+     ...
+     return float x;
+    }
+    
+   -public void calculateGPA(float v1, float v2)
+    {
+     ...
+     return int y;
+    }
+    
+    -Despite of the arguments (float n1, float n2 / float v1, float v2) in the two methods having different names, their types(float) and 
+     quantity(2 in the first method and 2 in the second method) are the same, making the signature in both methods the same.
+     
+ 
+*/
+
+
+//====================================================================================
