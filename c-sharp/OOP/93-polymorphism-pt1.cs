d821dd8a0323894fe107aefe6ba10bca2dd26084
Raphael Torres <contato.raphaeltorres@gmail.com>
2017-03-16 21:09:06 -0300

    Update 93-polymorphism-pt1.cs

--- c-sharp/OOP/93-polymorphism-pt1.cs
+++ c-sharp/OOP/93-polymorphism-pt1.cs
@@ -6,7 +6,8 @@
   -Every method can or cannot receive arguments(parameters). The quantity and types of it's arguments will define
   it's signature.
   -Every method has it own signature.
-  -Example: 
+  
+   -Example: 
    -public void calculateGPA(float n1, float n2)
     {
      ...
@@ -19,8 +20,14 @@
      return int y;
     }
     
-    -Despite of the arguments (float n1, float n2 / float v1, float v2) in the two methods having different names, their types(float) and 
-     quantity(2 in the first method and 2 in the second method) are the same, making the signature in both methods the same.
+   -Despite of the arguments (float n1, float n2 / float v1, float v2) in the two methods having different names, their types(float, float)
+   and quantity(2 in the first method and 2 in the second method) are the same, making the signature in both methods the same.
+     
+   -public void calculateGPA(int term, float n1, float n2)
+   {
+    ...
+   }
+   -The method above have 3 arguments(term, n1, n2) and 3 types (int, float, float) and it is different from the two mthods above.
      
  
 */
